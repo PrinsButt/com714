79cf4c29c09c454aa63fefddb9f9b0b14c3a9dea
Prins Butt <prinsbutt@gmail.com>
2021-02-04 11:07:27 +0000

    Completed question 1.

---
+++ practise/main.py
@@ -0,0 +1,9 @@
+# question 1
+def q1():
+    print("Who broke my heart?")
+    response = input()
+    print(f"My first love, {response}, broke my heart for the first time.")
+
+
+# call functions
+q1()
